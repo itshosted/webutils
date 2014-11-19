caac05c90156f453997af0d7713e7478c5c029eb
root <root@localhost.localdomain>
2014-11-19 18:46:49 +0100

    Cleanup. Refactoring session and using new encrypt-lib

--- session/main.go
+++ session/main.go
@@ -1,4 +1,4 @@
-package utils
+package session
 
 /**
  * Client-side session cookie.
@@ -10,18 +10,12 @@ package utils
  *  don't need talk to each other.
  */
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/json"
 	"errors"
-	"io"
 	"net/http"
-	"net/url"
 	"webutils/report"
 	"strconv"
 	"time"
+	"webutils/encrypt"
 )
 
 /**
@@ -41,80 +35,11 @@ type Session struct {
 }
 
 func (s *Session) Encrypt(iv string) (string, error) {
-	if len(iv) != 32 {
-		return "", errors.New("IV must be random 32-chars")
-	}
-	key := []byte(iv)
-	json, e := json.Marshal(s)
-	if e != nil {
-		return "", e
-	}
-
-	raw, e := s.encrypt(key, []byte(json))
-	if e != nil {
-		return "", e
-	}
-	return url.QueryEscape(base64.StdEncoding.EncodeToString(raw)), nil
+	return encrypt.EncryptBase64(iv, s)
 }
 
 func (s *Session) Decrypt(in string, iv string) error {
-	if len(iv) != 32 {
-		return "", errors.New("IV must be random 32-chars")
-	}
-	key := []byte(iv)
-
-	b, e := url.QueryUnescape(in)
-	if e != nil {
-		return e
-	}
-	str, e := base64.StdEncoding.DecodeString(b)
-	if e != nil {
-		return e
-	}
-	raw, e := s.decrypt(key, str)
-	if e != nil {
-		return e
-	}
-
-	if e := json.Unmarshal(raw, s); e != nil {
-		return e
-	}
-	return nil
-}
-
-func (s *Session) encrypt(key, text []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	b := base64.StdEncoding.EncodeToString(text)
-	ciphertext := make([]byte, aes.BlockSize+len(b))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
-	return ciphertext, nil
-}
-
-func (s *Session) decrypt(key, text []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	if len(text) < aes.BlockSize {
-		return nil, errors.New("ciphertext too short")
-	}
-	iv := text[:aes.BlockSize]
-	text = text[aes.BlockSize:]
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	cfb.XORKeyStream(text, text)
-	data, err := base64.StdEncoding.DecodeString(string(text))
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
+	return encrypt.DecryptBase64(iv, in, s)
 }
 
 func ExpireSession(w http.ResponseWriter, cookie *http.Cookie) {
