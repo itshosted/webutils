6f60b97c5af54d42346265c71f2837a05d454119
MP Droog <mark@xsnews.nl>
2015-04-02 08:50:15 +0200

    Feature. Introduce httpreq for logging http-requests (useful for debugging)

---
+++ httpreq/httpreq.go
@@ -0,0 +1,22 @@
+package httpreq
+
+/**
+ * Log HTTP-requests.
+ */
+import (
+	"github.com/xsnews/webutils/middleware"
+	"net/http"
+	"github.com/xsnews/microservice-core/log"
+)
+
+// Write HTTP request to log.
+func Use() middleware.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) bool {
+		form := ""
+		if r.PostForm != nil {
+			form = r.PostForm.Encode()
+		}
+		log.Println("httpreq: %s %s %s (IP=%s)(Form=%s)", r.Proto, r.Method, r.URL, r.RemoteAddr, form)
+		return true
+	}
+}
