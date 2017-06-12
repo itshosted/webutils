dc177fc0228ed7b383ab6b1a5b45a7a88b75038f
MP Droog <mpdroog@icloud.com>
2017-06-12 16:19:15 +0200

    Bugfix. Remove double base64, Bugfix. Cipher.CFB is considered unsafe (moved to Cipher.GCM)

--- encrypt/aes.go
+++ encrypt/aes.go
@@ -4,42 +4,48 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64" // TODO: Should not be here?
 	"errors"
 	"io"
 )
 
-func aesEncrypt(key, text []byte) ([]byte, error) {
+func aesEncrypt(key, plaintext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	b := base64.StdEncoding.EncodeToString(text)
-	ciphertext := make([]byte, aes.BlockSize+len(b))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
 		return nil, err
 	}
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
-	return ciphertext, nil
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
-func aesDecrypt(key, text []byte) ([]byte, error) {
+func aesDecrypt(key, ciphertext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	if len(text) < aes.BlockSize {
-		return nil, errors.New("aesDecrypt: ciphertext too short")
-	}
-	iv := text[:aes.BlockSize]
-	text = text[aes.BlockSize:]
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	cfb.XORKeyStream(text, text)
-	data, err := base64.StdEncoding.DecodeString(string(text))
+
+	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
-	return data, nil
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return nil, errors.New("malformed ciphertext")
+	}
+
+	return gcm.Open(nil,
+		ciphertext[:gcm.NonceSize()],
+		ciphertext[gcm.NonceSize():],
+		nil,
+	)
 }
---
+++ encrypt/main_test.go
@@ -0,0 +1,48 @@
+package encrypt
+
+import (
+	"testing"
+)
+
+type TestStruct struct {
+	Msg    string
+	Status bool
+}
+
+func TestInvalidEncryptBase64(t *testing.T) {
+	in := TestStruct{"Hello world!", true}
+	_, e := EncryptBase64("aes", "too_short", &in)
+	if e.Error() == "IV must be random 32-chars" {
+		return
+	}
+
+	t.Fatal(e)
+}
+
+func TestValidEncryptBase64(t *testing.T) {
+	iv := "12345678912345678912345678900000"
+
+	tests := []TestStruct{
+		TestStruct{"Hello world!", true},
+		TestStruct{"DERP", false},
+	}
+
+	for _, test := range tests {
+		s, e := EncryptBase64("aes", iv, &test)
+		if e != nil {
+			t.Fatal(e)
+		}
+
+		res := TestStruct{}
+		if e := DecryptBase64("aes", iv, s, &res); e != nil {
+			t.Fatal(e)
+		}
+
+		if res.Msg != test.Msg {
+			t.Errorf("res.Msg doesn't match: " + res.Msg)
+		}
+		if res.Status != test.Status {
+			t.Errorf("t.Status doesn't match")
+		}
+	}
+}
