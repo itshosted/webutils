9868a0e3c8ba13b96aa24d56fb02c0acd7dda18a
mark <mpdroog@rootdev.nl>
2023-01-20 10:47:04 +0100

    Patch. Warn and offer new func to solve racing issue in randtext

--- str/rand.go
+++ str/rand.go
@@ -1,6 +1,7 @@
 package str
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -18,6 +19,28 @@ var src = rand.NewSource(time.Now().UnixNano())
 // TODO: src.Func is NOT concurrent safe, so please don't use this function!!
 // http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
 func RandText(n int) string {
+    fmt.Printf("WARN: webutils.RandText (is NOT concurrently safe), please switch to webutils.RandText2\n");
+    b := make([]byte, n)
+    // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+        if remain == 0 {
+            cache, remain = src.Int63(), letterIdxMax
+        }
+        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+            b[i] = letterBytes[idx]
+            i--
+        }
+        cache >>= letterIdxBits
+        remain--
+    }
+
+    return string(b)
+}
+
+// Random string chars, src=source n=length
+// Example src = rand.NewSource(time.Now().UnixNano())
+// DevNote: src is NOT concurrently safe, so be aware!
+func RandText2(src rand.Source, n int) string {
     b := make([]byte, n)
     // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
     for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
--- str/rand_test.go
+++ str/rand_test.go
@@ -2,6 +2,8 @@ package str
 
 import (
 	"testing"
+	"time"
+	"math/rand"
 )
 
 func TestRandText(t *testing.T) {
@@ -14,4 +16,17 @@ func TestRandText(t *testing.T) {
 			t.Fatalf("RandText uniq fail in round %d", i)
 		}
 	}
+}
+
+func TestRandText2(t *testing.T) {
+	src := rand.NewSource(time.Now().UnixNano())
+        uniq := make(map[string]bool)
+        for i := 0; i < 5000; i++ {
+                key := RandText2(src, 32)
+                if _, ok := uniq[key]; !ok {
+                        uniq[key] = true
+                } else {
+                        t.Fatalf("RandText2 uniq fail in round %d", i)
+                }
+        }
 }
